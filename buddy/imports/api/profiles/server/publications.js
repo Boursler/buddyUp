45bbfa9da6bc0ddb830ab09eaa219818d128c299
Briana Oursler <briana.oursler@gmail.com>
2018-07-20 14:04:24 -0700

    Fix reference to profiles link and correct import of Profile

--- buddy/imports/api/profiles/server/publications.js
+++ buddy/imports/api/profiles/server/publications.js
@@ -1,15 +1,16 @@
 import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
-import {Profiles} from '../profiles.js';
+import {Profiles} from '../profiles';
 
 
 Meteor.publish('profiles', function profilesPublication() {
+    console.log("inside the publication now");
     return Profiles.find({
             userId: this.userId ,
         });
   });
 //goal is to use cultofcoders:grapher to get linked documents and expose them
   Meteor.publish('buddies', function eventBuddiesPublication(){
-      const buddyLink = Profiles.getLink(profileId, 'profiles'); 
+      const buddyLink = Profiles.getLink(profileId, 'events');
       return buddyLink.find().fetch();
   })
