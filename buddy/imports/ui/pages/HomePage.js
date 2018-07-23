6140358e7e7c506142a55e99d39f3509ecec4be1
sepideh <sepidehfallah2@gmail.com>
2018-07-23 12:53:10 -0700

    final touch of our mistion on the homepage

--- buddy/imports/ui/pages/HomePage.js
+++ buddy/imports/ui/pages/HomePage.js
@@ -218,14 +218,13 @@ const HomepageLayout = () => (
         <Grid.Row>
           <Grid.Column width={8}>
             <Header as='h3' style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
+              Our Mission 
             </Header>
             <p style={{ fontSize: '1.33em' }}>
-              We can give your company superpowers to do things that they never thought possible.
-              Let us delight your customers and empower your needs... through pure data analytics.
+            To make a cooperative arrangement whereby individuals are teamed up and assume responsibility for one another’s welfare
             </p>
             <Header as='h3' style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
+            Buddy Up: Enrich your life and improve your health
             </Header>
             <p style={{ fontSize: '1.33em' }}>
               Yes that's right, you thought it was the stuff of dreams, but even bananas can be
@@ -243,17 +242,50 @@ const HomepageLayout = () => (
         </Grid.Row>
       </Grid>
     </Segment>
-    
-    <Segment style={{ padding: '0em' }} vertical>
-    <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Container text>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+        Can't find a buddy who is available Wednesday evenings to attend an event in your local park? Want to run with a buddy willing to wake up early in the morning?
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+        Any Buddy up is dedicated to making it easy for people to interact with each other, primarily through recreational activities. Getting together to bike, golf, sail, hike and more is a cinch with buddyup.
+        </p>
+        <Button as='a' size='large'>
+          Sign Up
+        </Button>
         <Divider
           as='h4'
           className='header'
           horizontal
           style={{ margin: '3em 0em', textTransform: 'uppercase' }}
         >
-          <a href='#'>About Us</a>
+          <a href='#'>Case Studies</a>
         </Divider>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+       ABCDEFGH IJKL
+
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+        We wanted to create our own awesome buddy system – Growth Socializing style. So, ‘Any Buddy Up?!’ was born!
+        </p>
+        {/* <Button as='a' size='large'>
+          I'm Still Quite Interested
+        </Button> */}
+      </Container>
+    </Segment>
+
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Container text>
+        <Header as='h3'  style={{ fontSize: '2em', Position: 'center' }}>
+         About Us :  AWESOME ALLIES IN ACTION
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+        We wanted to create our own awesome buddy system – Growth Socializing style. So, ‘Any Buddy Up?!’ was born!
+        </p>
+      </Container>
+    </Segment>
+    
+    <Segment style={{ padding: '0em' }} vertical> 
       <Grid celled='internally' columns='equal' stackable>
         <Grid.Row textAlign='center'>
         <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
@@ -271,15 +303,14 @@ const HomepageLayout = () => (
               "I shouldn't have gone with their competitor."
             </Header>
             <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src='/images/Categories/.jpg' />
-              <a href=''>NAN</a> Chief Fun Officer Acme Toys
+              <Image avatar src='/images/Categories/briana.png' />
+              <a href='https://www.linkedin.com/in/briana-oursler/'>Briana</a> Chief Fun Officer Acme Toys
             </p>
             <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Segment>
-
     <Segment style={{ padding: '0em' }} vertical>
       <Grid celled='internally' columns='equal' stackable>
         <Grid.Row textAlign='center'>   
@@ -307,40 +338,6 @@ const HomepageLayout = () => (
       </Grid>
     </Segment>
 
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as='h3' style={{ fontSize: '2em' }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Instead of focusing on content creation and hard work, we have learned how to master the
-          art of doing nothing by providing massive amounts of whitespace and generic content that
-          can seem massive, monolithic and worth your attention.
-        </p>
-        <Button as='a' size='large'>
-          Read More
-        </Button>
-        <Divider
-          as='h4'
-          className='header'
-          horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-        >
-          <a href='#'>Case Studies</a>
-        </Divider>
-        <Header as='h3' style={{ fontSize: '2em' }}>
-          Did We Tell You About Our Bananas?
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-          it's really true. It took years of gene splicing and combinatory DNA research, but our
-          bananas can really dance.
-        </p>
-        <Button as='a' size='large'>
-          I'm Still Quite Interested
-        </Button>
-      </Container>
-    </Segment>
     <Segment inverted vertical style={{ padding: '5em 0em' }}>
       <Container>
         <Grid divided inverted stackable>
